c0521c14d5ace0581b68df1ca67870316ebeaa3d
Dylan Cochran <dylan.cochran@onyxpoint.com>
2017-04-10 14:04:16 -0400

    Fix file distribution

--- manifests/consul.pp
+++ manifests/consul.pp
@@ -8,17 +8,28 @@ class libkv::consul(
   $bootstrap = false,
   $key = undef,
   $version = '0.8.0',
-  $client_addr = '0.0.0.0',
 ) {
   package { "unzip": }
   if ($bootstrap == true) {
     $bootstrap_expect = 1
   }
+  $keypath = '/etc/simp/bootstrap/consul/key'
+  $master_token_path = '/etc/simp/bootstrap/consul/master_token'
   if ($server == true)
     $cert_file_name = '/etc/simp/bootstrap/consul/server.dc1.consul.cert.pem'
     $private_file_name = '/etc/simp/bootstrap/consul/server.dc1.consul.private.pem'
     $ca_file_name = '/etc/simp/bootstrap/consul/ca.pem'
   }
+  file { $cert_file_name:
+    content => file($cert_file_name)
+  }
+  file { $private_file_name:
+    content => file($private_file_name)
+  }
+  file { $private_file_name:
+    content => file($private_file_name)
+  }
+
   $hash = lookup('consul::config_hash', { "default_value" => {} })
   $class_hash =     {
       'data_dir'         => '/opt/consul',
@@ -27,7 +38,6 @@ class libkv::consul(
       'node_name'        => $::hostname,
       'retry_join'       => [ $serverip ],
       'advertise_addr'   => $::ipaddress,
-      'client_addr'      => $client_addr,
       'ui_dir'           => '/opt/consul/ui',
   }
   $merged_hash = $class_hash.merge($hash)
