b5bbff243fce2a79e76862f7c10d8741cdb0295f
Dylan Cochran <dylan.cochran@onyxpoint.com>
2017-04-10 13:17:17 -0400

    Add consul provisioner

---
+++ bootstrap/consul.pp
@@ -0,0 +1,33 @@
+include libkv::consul
+package { "unzip":
+} ->
+file { "/etc/simp":
+	ensure => directory,
+} ->
+file { "/etc/simp/bootstrap/":
+	ensure => directory,
+} ->
+file { "/etc/simp/bootstrap/consul":
+	ensure => directory,
+} ->
+exec { "/usr/bin/uuidgen >/etc/simp/bootstrap/consul/master_token":
+	creates => '/etc/simp/bootstrap/consul/master_token',
+} ->
+exec { "/opt/puppetlabs/bin/puppet cert generate server.dc1.consul":
+	creates => '/etc/puppetlabs/puppet/ssl/private_keys/server.dc1.consul.pem',
+} ->
+file { "/etc/simp/bootstrap/consul/server.dc1.consul.private.pem":
+source => '/etc/puppetlabs/puppet/ssl/private_keys/server.dc1.consul.pem',
+} ->
+file { "/etc/simp/bootstrap/consul/server.dc1.consul.cert.pem":
+source => '/etc/puppetlabs/puppet/ssl/certs/server.dc1.consul.pem',
+} ->
+file { "/etc/simp/bootstrap/consul/ca.pem":
+source => '/etc/puppetlabs/puppet/ssl/ca/ca_crt.pem',
+} -> 
+Class["consul"] -->
+exec { "/usr/local/bin/consul keygen >/etc/simp/bootstrap/consul/key":
+  path => $::path,
+  creates => '/etc/simp/bootstrap/consul/key',
+}
+
